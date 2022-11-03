957e72def5fae11137e08f5270d0b823be164c1b
WeZaPro <taweesak9359@gmail.com>
2022-11-03 08:51:13 +0700

    up1

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+let port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // require("./routes/routers")(app);
 
-app.listen("5000", () => {
-  console.log("server run port 5000");
+app.listen(port, () => {
+  console.log(`server run port ${port}`);
 });
 
 app.get("/", (req, res) => {
