68d602b7908f3ad572fecdd3c3d758f21cb8db5c
WeZaPro <taweesak9359@gmail.com>
2022-11-03 09:44:49 +0700

    update02

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "sabuyme-secret-key",
+};
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const dbConfig = require("./config/db.config");
+const db = require("./models");
+const Role = db.role;
 let port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// require("./routes/routers")(app);
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./routes/auth.routes")(app);
 
 app.listen(port, () => {
   console.log(`server run port ${port}`);
@@ -15,3 +27,53 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.json({ message: "Server is successed." });
 });
+
+// MongoDb
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "superUser",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'superUser' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
